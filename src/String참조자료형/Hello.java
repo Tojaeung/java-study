0682bad51c463f9b41c19d5ad41c3eb38b09ed52
Tojaeung <kimwoon11@naver.com>
2022-12-12 23:38:39 +0900

    String 참조자료형 포스팅

---
+++ src/String참조자료형/Hello.java
@@ -0,0 +1,15 @@
+package String참조자료형;
+
+public class Hello {
+	public static void main(String[] args) {
+		String str1 = new String("안녕");
+		String str2 = str1;
+
+		// String 객체내의 값 변경시 새로운 객체를 생성한다.
+		str1 = "안녕하세요";
+
+		System.out.println(str1); // 안녕하세요
+		System.out.println(str2); // 안녕
+
+	}
+}
---
+++ src/String참조자료형/Hello3.java
@@ -0,0 +1,14 @@
+package String참조자료형;
+
+public class Hello3 {
+	public static void main(String[] args) {
+
+		/*
+		 * String 객체의 값은 바꿀수 없기때문에
+		 * "안녕하세요!" 문자열 값을 만들기 위해
+		 * 힙 영역에서 5개의 String 객체가 만들어진다.
+		 */
+		String str1 = "안녕" + "하세요" + "!";
+		System.out.println(str1);
+	}
+}
