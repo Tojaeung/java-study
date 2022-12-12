a50320bc1afddb4e122faedf8d552d19c39f3939
Tojaeung <kimwoon11@naver.com>
2022-12-13 03:07:45 +0900

    Class 내부구성요소

---
+++ src/Class내부구성요소/B.java
@@ -0,0 +1,17 @@
+package Class내부구성요소;
+
+public class B {
+	private int m;
+	private int n;
+
+	public static void main(String[] args) {
+		B b = new B();
+		System.out.println(b.m); // 0
+		System.out.println(b.n); // 0
+	}
+
+	private void work1() {
+		int k;
+		// System.out.println(k); // 오류
+	}
+}
