c6713eedb5d4719d9b00d3c084c846b96cf6af4c
Tojaeung <kimwoon11@naver.com>
2022-12-14 17:02:42 +0900

    상속과 다형성(part.2)

--- src/ch4_상속과다형성/part1/Human.java
+++ src/ch4_상속과다형성/part1/Human.java
@@ -13,7 +13,7 @@ public class Human {
 	public static void main(String[] args) {
 		Student student = new Student(); // 학생은 학생이다.
 		Human student2 = new Student(); // 학생은 인간이다.
-		Student Human = new Human(); // 인간은 학생이다. (에러)
+		// Student Human = new Human(); // 인간은 학생이다. (에러)
 
 		System.out.println(student.name); // 토재웅
 		System.out.println(student.age); // 12
