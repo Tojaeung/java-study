1b4c4fed67144dfc28bd65604361803d15185226
Tojaeung <kimwoon11@naver.com>
2022-12-15 01:29:55 +0900

    상속과 다형성(part.4)

---
+++ src/ch4_상속과다형성/part4/Main.java
@@ -0,0 +1,21 @@
+package ch4_상속과다형성.part4;
+
+public class Main {
+	public static void main(String[] args) {
+		// 바깥 객체부터 탐색한다.
+		B b = new B();
+		System.out.println(b.n); // 3
+
+		// 인스턴스 필드는 오버라이딩 되지 않는다.
+		A a = new B();
+		System.out.println(a.n); // 2
+	}
+}
+
+class A {
+	int n = 2;
+}
+
+class B extends A {
+	int n = 3;
+}
---
+++ src/ch4_상속과다형성/part4/Main3.java
@@ -0,0 +1,24 @@
+package ch4_상속과다형성.part4;
+
+public class Main3 {
+	public static void main(String[] args) {
+		B b = new B();
+		b.print(); // B 클래스 print 메서드
+
+		// 오버라이딩 되지 않는다.
+		A a = new B();
+		a.print(); // A 클래스 print 메서드
+	}
+}
+
+class A {
+	static void print() {
+		System.out.println("A 클래스 print 메서드");
+	};
+}
+
+class B extends A {
+	static void print() {
+		System.out.println("B 클래스 print 메서드");
+	};
+}
