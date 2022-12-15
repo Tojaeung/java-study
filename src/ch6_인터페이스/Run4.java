b8bcc4c4ab52d9b33ec4d60d7b05a3309af54a0d
Tojaeung <kimwoon11@naver.com>
2022-12-16 02:44:56 +0900

    인터페이스

---
+++ src/ch6_인터페이스/Run4.java
@@ -0,0 +1,15 @@
+package ch6_인터페이스;
+
+interface A {
+	static void print() {
+		System.out.println("안녕~");
+	}
+}
+
+public class Run4 {
+	public static void main(String[] args) {
+		// 객체 생성없이 바로 접근
+		// 클래스 내부 정적 메서드와 동일
+		A.print();
+	}
+}
