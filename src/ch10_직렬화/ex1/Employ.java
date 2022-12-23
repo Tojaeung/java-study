62140c3cdfc980c6a67c98f9a4388a22edb08fa9
Tojaeung <kimwoon11@naver.com>
2022-12-23 19:17:47 +0900

    직렬화

---
+++ src/ch10_직렬화/ex1/Employ.java
@@ -0,0 +1,21 @@
+package ch10_직렬화.ex1;
+
+import java.io.Serializable;
+
+public class Employ implements Serializable {
+	String name;
+	int age;
+	String position;
+
+	public Employ(String name, int age, String position) {
+		this.name = name;
+		this.age = age;
+		this.position = position;
+	}
+
+	@Override
+	public String toString() {
+		return "Employ [name=" + name + ", age=" + age + ", position=" + position + "]";
+	}
+
+}
